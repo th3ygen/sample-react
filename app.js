f1b086d856178bc057815c48e9b34704012d4934
Aidil <th3ygen@gmail.com>
2022-01-28 19:30:53 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("/test", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+	console.log("Server is running on port 8080");
+});
